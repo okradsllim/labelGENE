78d5aa77176decec6121a105c0ebec9159119c83
okradsllim <william.ayesu.nyarko@gmail.com>
2024-03-17 17:17:15 -0400

    Updated the docs/conf.py file to include the new theme.

--- docs/conf.py
+++ docs/conf.py
@@ -1,26 +1,47 @@
-# Configuration file for the Sphinx documentation builder.
+# docs/conf.py
 
-extensions = ['sphinx.ext.autodoc']
+import os
+import sys
 
-# Suffix of source filenames.
-source_suffix = '.rst'
+# Project root directory as the parent of 'docs'
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Adding the project root directory to sys.path
+sys.path.insert(0, project_root)
+
+# Sphinx extension module names here, as strings.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # Napoleon extension for Google-style docstrings
+]
+
+# Set autodoc options
+autodoc_mock_imports = ['win32com']
+autodoc_default_options = {
+    'members': True,
+    'undoc-members': True,
+    'private-members': True,
+    'special-members': '__init__',
+    'imported-members': True,
+}
 
 # Master toctree document.
 master_doc = 'index'
 
-# General info
-project = 'labelGENE'
+# General information
+project = 'Your Project'
 copyright = '2023, Will Nyarko'
 author = 'Will Nyarko'
 
 # Version and release
 version = '0.5'
-release = '0.5'
+release = '1.0'
 
+# directories to ignore when looking for source files.
 exclude_patterns = ['_build']
 
-# Pygments (syntax highlighting)
+# Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-# HTML theme
+# Theme for HTML and HTML Help pages.
 html_theme = 'sphinx_rtd_theme'
